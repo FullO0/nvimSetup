a460447bf59b5d6d57c1cf5397c95992da2be525
Christian Jacobus van der Walt <Christian@vanderwalts.net>
2026-02-23 17:35:50 +0200

    feat: Add kemaps to copilot

--- init.lua
+++ init.lua
@@ -573,7 +573,18 @@ require('lazy').setup({
     event = 'InsertEnter',
     config = function()
       require('copilot').setup {
-        suggestion = { enabled = true, auto_trigger = true },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = '<Tab>',
+            accept_word = '<C-Right>',
+            accept_line = '<C-Down>',
+            next = '<M-]>',
+            prev = '<M-[>',
+            dismiss = '<C-]>',
+          },
+        },
         panel = { enabled = false },
       }
     end,
