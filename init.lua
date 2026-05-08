ef18ae1cd863a86574ae314642c67e1c76426a07
Christian Jacobus van der Walt <Christian@vanderwalts.net>
2026-05-08 14:34:28 +0200

    fix: Format messing with code folding

--- init.lua
+++ init.lua
@@ -1187,13 +1187,14 @@ require('lazy').setup({
       {
         '<leader>f',
         function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
+          require('conform').format { async = false, lsp_format = 'fallback' }
         end,
         mode = '',
         desc = '[F]ormat buffer',
       },
     },
     opts = {
+      async = false,
       notify_on_error = false,
       format_on_save = function(bufnr)
         local disable_filetypes = { c = true, cpp = true }
