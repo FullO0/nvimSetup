ba3ecd2404a7ff72d187daad7cc8c47467100c5c
Christian van der Walt <christian@vanderwalts.net>
2026-08-07 08:58:09 +0200

    feat: add xml filetype settings

--- lua/config/filetypes.lua
+++ lua/config/filetypes.lua
@@ -92,6 +92,16 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
   end,
 })
 
+-- XML specific settings
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  pattern = { 'xml' },
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end,
+})
+
 -- Web file specific settings
 vim.api.nvim_create_autocmd({ 'FileType' }, {
   pattern = { 'json', 'javascript', 'javascriptreact', 'css', 'html' },
